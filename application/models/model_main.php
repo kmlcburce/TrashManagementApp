740d429355cd737845fc436b5085a87c1d7c173f
sojuComar <49076464+sojuComar@users.noreply.github.com>
2019-07-09 19:03:40 +0800

    added function for beneficiary

--- application/models/model_main.php
+++ application/models/model_main.php
@@ -5,19 +5,18 @@
 
 	class model_admin extends CI_model{
 
+	public function get_item(){
+		$this->load->model("model_main");
+		}
+		
+	public function bene_register(){
+			$data = array(
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email')
+			);
 
-	function get_item(){
-
-	$this->load->model("model_main");
-	
-
-	}
-
-
-
-
-
-
+			$this->db->insert('user_accounts', $data);
+		}
 
 	}
 
