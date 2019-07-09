a5b80f5587ea39dd6d300c2e18cbebfd20d54df7
Kyle Matthews <kmlcburce@gmail.com>
2019-07-09 02:40:45 -0700

    added stuff

---
+++ application/views/client/client_index.php
@@ -0,0 +1,97 @@
+<body>
+
+	<nav class="navbar navbar-expand-sm" style="background-color: #fbe8a6; font-size: 18px;letter-spacing: 1px">
+		  <ul class="navbar-nav">
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">ABOUT</a>
+		    </li> 
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">FAQ</a>
+		    </li>
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">DONATE</a>
+		    </li>
+
+		  </ul>
+	</nav>
+
+	<br>
+	<br>
+	<br>
+
+	<div class="container">
+				<div class="card-columns">
+					<div class="container">
+						<div class="container"> 
+							<div class="container">
+
+					 <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"> <a href="#" class="stretched-link">CARCAR CITY<br></a></h5>
+				      </div>
+				    </div>
+
+					<div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				        <h5 class="card-title"> <a href="#" class="stretched-link">CEBU CITY <br></a></h5>
+				      </div>
+				    </div>
+
+				     <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"> <a href="#" class="stretched-link">DANAO CITY<br></a></h5>
+				      </div>
+				    </div>
+
+				     <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"> <a href="#" class="stretched-link">LAPU-LAPU CITY<br></a></h5>
+				      </div>
+				    </div>
+
+				    <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				        <h5 class="card-title"> <a href="#" class="stretched-link">MANDAUE CITY <br></a></h5>
+				      </div>
+				    </div>
+
+				     <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"> <a href="#" class="stretched-link">MINGLANILLA CEBU<br></a></h5>
+				      </div>
+				    </div>
+
+				    <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"><a href="#" class="stretched-link">NAGA CITY <br></a></h5>
+				      </div>
+				    </div>
+
+				     <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"><a href="#" class="stretched-link">TALISAY CITY<br></a></h5>
+				      </div>
+				    </div>
+
+				     <div class="card" style="width: 15rem;">
+				      <div class="card-body text-center">
+				       <h5 class="card-title"><a href="#" class="stretched-link">TOLEDO CITY<br></a></h5>
+				      </div>
+				    </div>
+
+				    </div>
+				    </div>
+				</div>
+				</div> 
+				
+			
+			<br> <br> 
+
+				<div style="border-color:#d2fdff;border-width: 3px; border-style: solid"></div>
+
+				<!-- RESULT GOES HERE --> 
+			
+	</div>
