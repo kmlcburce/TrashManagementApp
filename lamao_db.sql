9e5c03257175abd44d3d31331a4fe886d0725bbd
Kyle Matthews <kmlcburce@gmail.com>
2019-07-10 03:59:04 -0700

    updated db

--- lamao_db.sql
+++ lamao_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2019 at 11:55 AM
+-- Generation Time: Jul 10, 2019 at 12:58 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.21
 
@@ -23,98 +23,152 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `donor`
+-- Table structure for table `beneficiary_form`
 --
 
-CREATE TABLE `donor` (
-  `d_id` int(255) NOT NULL,
-  `d_name` varchar(255) NOT NULL,
-  `d_email` varchar(255) NOT NULL,
-  `d_password` varchar(255) NOT NULL,
-  `d_establishment` varchar(256) NOT NULL,
-  `f_id` int(255) NOT NULL
+CREATE TABLE `beneficiary_form` (
+  `bf_id` int(8) NOT NULL,
+  `bf_product_amount` int(8) NOT NULL,
+  `bf_data_ordered` date NOT NULL,
+  `u_id` int(8) NOT NULL,
+  `in_id` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `form`
+-- Table structure for table `donor_form`
 --
 
-CREATE TABLE `form` (
-  `f_id` int(11) NOT NULL,
-  `i_id` int(11) NOT NULL,
-  `f_quantity` varchar(255) NOT NULL,
-  `f_city` varchar(255) NOT NULL
+CREATE TABLE `donor_form` (
+  `df_id` int(11) NOT NULL,
+  `u_id` int(8) NOT NULL,
+  `df_import_data` date NOT NULL,
+  `in_id` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `item`
+-- Table structure for table `item_list`
 --
 
-CREATE TABLE `item` (
-  `i_id` int(11) NOT NULL,
-  `i_name` varchar(255) NOT NULL,
-  `i_category` enum('Noodles','Canned Goods','Canned Drinks','Bottled Water') NOT NULL,
-  `c_exp_date` date NOT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `item_list` (
+  `il_id` int(11) NOT NULL,
+  `il_name` varchar(255) NOT NULL,
+  `il_category` enum('Noodles','Canned Goods','Canned Drinks','Bottled Water') NOT NULL,
+  `il_exp_date` date NOT NULL,
+  `i_total_quantity` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `item`
+-- Dumping data for table `item_list`
 --
 
-INSERT INTO `item` (`i_id`, `i_name`, `i_category`, `c_exp_date`, `reg_date`) VALUES
-(1, 'Homie Chicken Noodles', 'Noodles', '2019-11-08', '2019-07-08 09:27:44'),
-(2, 'Lucky Me! Pancit Canton - Calamansi', 'Noodles', '2019-12-12', '2019-07-08 09:27:44'),
-(3, 'Carne Norte Corned Beef', 'Canned Goods', '2020-04-22', '2019-07-08 09:28:48'),
-(4, 'Del Monte Canned Pineapple Juice', 'Canned Drinks', '2020-05-28', '2019-07-08 09:28:48'),
-(5, 'Lucky Me! Beef Noodles', 'Noodles', '2020-07-22', '2019-07-08 09:31:07'),
-(6, 'Nature Spring Bottled Water', '', '2020-09-29', '2019-07-08 09:31:07');
+INSERT INTO `item_list` (`il_id`, `il_name`, `il_category`, `il_exp_date`, `i_total_quantity`) VALUES
+(1, 'Homie Chicken Noodles', 'Noodles', '2019-11-08', 0),
+(2, 'Lucky Me! Pancit Canton - Calamansi', 'Noodles', '2019-12-12', 0),
+(3, 'Carne Norte Corned Beef', 'Canned Goods', '2020-04-22', 0),
+(4, 'Del Monte Canned Pineapple Juice', 'Canned Drinks', '2020-05-28', 0),
+(5, 'Lucky Me! Beef Noodles', 'Noodles', '2020-07-22', 0),
+(6, 'Nature Spring Bottled Water', '', '2020-09-29', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_needed`
+--
+
+CREATE TABLE `item_needed` (
+  `in_id` int(8) NOT NULL,
+  `i_id` int(8) NOT NULL,
+  `in_quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `u_id` int(11) NOT NULL,
+  `u_fname` char(255) NOT NULL,
+  `u_lname` char(255) NOT NULL,
+  `u_email` varchar(255) NOT NULL,
+  `u_password` varchar(255) NOT NULL,
+  `u_city` enum('Carcar City','Cebu City','Danao City','Lapu-Lapu City','Mandaue City','Minglanilia Cebu','Naga Cebu','Talisay City','Toledo City') NOT NULL,
+  `u_type` enum('admin','donor','beneficiary','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `donor`
+-- Indexes for table `beneficiary_form`
 --
-ALTER TABLE `donor`
-  ADD PRIMARY KEY (`d_id`);
+ALTER TABLE `beneficiary_form`
+  ADD PRIMARY KEY (`bf_id`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `in_id` (`in_id`);
 
 --
--- Indexes for table `form`
+-- Indexes for table `donor_form`
 --
-ALTER TABLE `form`
-  ADD PRIMARY KEY (`f_id`);
+ALTER TABLE `donor_form`
+  ADD PRIMARY KEY (`df_id`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `in_id` (`in_id`);
 
 --
--- Indexes for table `item`
+-- Indexes for table `item_list`
 --
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`i_id`);
+ALTER TABLE `item_list`
+  ADD PRIMARY KEY (`il_id`);
+
+--
+-- Indexes for table `item_needed`
+--
+ALTER TABLE `item_needed`
+  ADD PRIMARY KEY (`in_id`),
+  ADD UNIQUE KEY `i_id` (`i_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`u_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `donor`
+-- AUTO_INCREMENT for table `beneficiary_form`
 --
-ALTER TABLE `donor`
-  MODIFY `d_id` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `beneficiary_form`
+  MODIFY `bf_id` int(8) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `form`
+-- AUTO_INCREMENT for table `donor_form`
 --
-ALTER TABLE `form`
-  MODIFY `f_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `donor_form`
+  MODIFY `df_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `item`
+-- AUTO_INCREMENT for table `item_list`
 --
-ALTER TABLE `item`
-  MODIFY `i_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `item_list`
+  MODIFY `il_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `item_needed`
+--
+ALTER TABLE `item_needed`
+  MODIFY `in_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
