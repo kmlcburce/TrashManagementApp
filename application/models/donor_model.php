40d4222e7e741c70a7728d90c25c2b65fbc3f6e8
Annika Tan <annikatan@Annikas-MacBook-Pro.local>
2019-08-07 19:33:52 +0800

    added donor functions

--- application/models/donor_model.php
+++ application/models/donor_model.php
@@ -9,5 +9,34 @@
 
 
          }
+
+    public function crDonor()
+	{
+		$data = array(
+
+			'd_email'		=> $this->input->post('emailD'),
+			'd_username' 	=> $this->input->post('usernameD'),
+			'd_password' 	=> $this->input->post('passwordD')
+
+		);
+
+		$this->db->insert('donor', $data);
+	}
+
+	function can_login($username,$password)
+	{
+		$this->db->where('username', $username);
+		$this->db->where('password', $password);
+		$query = $this->db->get('donor_accounts');
+
+		if($query->num_rows() > 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
     }
 ?>
