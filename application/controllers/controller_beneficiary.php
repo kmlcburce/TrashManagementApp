9b1a5ab3ca41751396820e66e9d1304be77d9eec
Annika Tan <annikatan@Annikas-MacBook-Pro.local>
2019-08-07 19:30:33 +0800

    added controllers for bene and donor
    registration and sessions

---
+++ application/controllers/controller_beneficiary.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class controller_beneficiary extends CI_Controller {
+
+	public function __construct
+	{
+		parent:: __construct();
+		$this->load->model('model_beneficiary');
+	}
+
+	public function index()
+	{
+		$this->load->view('bene_reglog');
+	}
+
+	public function createBeneficiary()
+	{
+		$this->model_beneficiary->crBeneficiary();
+
+		redirect("controller_beneficiary");
+	} 
+
+	function login()
+	{
+		//'http://localhost/wastage/controller_beneficiary/login'
+
+		$this->load->view("/bene/bene_reglog",$data);  
+
+	}
+
+	function login_validation()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('username','Username','required');
+		$this->form_validation->set_rules('password','Password','required');
+
+		if($this->form_validation->run())
+		{
+			//true
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+
+			//model function
+			$this->load->model('model_beneficiary');
+			if($this->main_model->can_login($username,$password))
+			{
+				$session_data = array(
+						'username' => $username
+				);
+				$this->session->set_userdata($session_data);
+				redirect(base_url() . 'controller_beneficiary/enter');
+			}
+			else
+			{
+				$this->session->set_flashdata('error','Invalid Username and Password');
+				redirect(base_url() . 'controller_beneficiary/login');
+			}
+		}
+		else
+		{
+			//false
+			$this->login();
+		}
+	}
+
+	function enter(){
+		if($this->session->userdata('username') != '')
+		{
+			echo '<label><a href="'.base_url().'controller_beneficiary/logout">Logout</a></label>'; //logout link
+		}
+		else
+		{
+			redirect(base_url() . 'controller_beneficiary/login');
+		}
+	}
+
+	function logout()
+	{
+		$this->session->unset_userdata('username');
+		redirect(base_url() . 'controller_beneficiary/login');
+	}
+}
